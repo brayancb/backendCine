aed3a93538b41bf170738b0ce235be7be0e634e1
brayancb <143953434+brayancb@users.noreply.github.com>
2025-05-17 22:05:07 -0400

    feature: habilitar peticiones front

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+    origin: "*", // Permite solo solicitudes CORS de este origen
+    methods: "GET,POST,PUT,DELETE", // Métodos HTTP permitidos
+    allowedHeaders: "Content-Type, Authorization", // Encabezados permitidos
+  });
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
